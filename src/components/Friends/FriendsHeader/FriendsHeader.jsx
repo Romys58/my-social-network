22538e9d457ed1395adec6513fbe26d745f6bff4
unknown <rom44ain.filippov@yandex.ru>
2022-04-13 22:53:46 +0300

    add data in tabsData

--- src/components/Friends/FriendsHeader/FriendsHeader.jsx
+++ src/components/Friends/FriendsHeader/FriendsHeader.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import style from "./FriendsHeader.module.css";
 import Tabs from "./Tabs/Tabs";
 
+// счетчик на вкладке friends {All Friends and Online friends} //
+const tabsData = [
+    {id: "1", counter: "36", text: "All friends"},
+    {id: "2", counter: "6", text: "Friends online"}
+]
+
 
 const FriendsHeader = () => {
     return(
         <div className={style.friends__header}>
-                <Tabs counter="36" id="1" text="All friends" />
-                <Tabs counter="6" id="2" text="Friends online" />
+                <Tabs id={tabsData [0].id} counter={tabsData [0].counter} text={tabsData [0].text} />
+                <Tabs id={tabsData [1].id} counter={tabsData [1].counter} text={tabsData [1].text} />
                 <button className={style.friends__button}>Find friends</button>
             </div >
     );
