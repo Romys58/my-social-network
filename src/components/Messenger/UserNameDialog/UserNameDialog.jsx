852756ae99662e322339ac88dc8a2f99152523c3
unknown <rom44ain.filippov@yandex.ru>
2022-04-13 20:02:12 +0300

    the new data

--- src/components/Messenger/UserNameDialog/UserNameDialog.jsx
+++ src/components/Messenger/UserNameDialog/UserNameDialog.jsx
@@ -3,17 +3,29 @@ import style from "./UserNameDialog.module.css";
 import UserNameItem from "./UserNameItem/UserNameItem";
 
 
+const userNameData = [
+    {name: "Evgenia", id: "1"},
+    {name: "Sveta", id: "2"},
+    {name: "Andrey", id: "3"},
+    {name: "Kirill", id: "4"},
+    {name: "Dmitriy", id: "5"},
+    {name: "Anton", id: "6"},
+    {name: "Kostya", id: "7"},
+    {name: "Olya", id: "8"}
+]
+
+
 const UserNameDialog = () => {
     return(
         <div className={style.user_name_dialog}>
-                        <UserNameItem name="Evgenia" id="1"/>
-                        <UserNameItem name="Sveta" id="2"/>
-                        <UserNameItem name="Andrey" id="3"/>
-                        <UserNameItem name="Kirill" id="4"/>
-                        <UserNameItem name="Dmitry" id="5"/>
-                        <UserNameItem name="Anton" id="6"/>
-                        <UserNameItem name="Kostya" id="7"/>
-                        <UserNameItem name="Olya" id="8"/>
+                        <UserNameItem name={userNameData [0].name} id={userNameData [0].id} />
+                        <UserNameItem name={userNameData [1].name} id={userNameData [1].id}/>
+                        <UserNameItem name={userNameData [2].name} id={userNameData [2].id}/>
+                        <UserNameItem name={userNameData [3].name} id={userNameData [3].id}/>
+                        <UserNameItem name={userNameData [4].name} id={userNameData [4].id}/>
+                        <UserNameItem name={userNameData [5].name} id={userNameData [5].id}/>
+                        <UserNameItem name={userNameData [6].name} id={userNameData [6].id}/>
+                        <UserNameItem name={userNameData [7].name} id={userNameData [7].id}/>
                     </div>
     );
 }
