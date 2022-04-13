9a082f100bec7c2faf18f86aec4929cf2fd01a31
unknown <rom44ain.filippov@yandex.ru>
2022-04-13 22:39:45 +0300

    add data in dialogContent

--- src/components/Messenger/DialogContent/DialogContent.jsx
+++ src/components/Messenger/DialogContent/DialogContent.jsx
@@ -2,32 +2,11 @@ import React from "react";
 import style from "./DialogContent.module.css";
 
 
-const DialogContent = () => {
+const DialogContent = (props) => {
     return (
         <div className={style.dialog_content}>
             <div className={style.item}>
-                Hi, aren't you studying react right now?
-            </div>
-            <div className={style.item}>
-                Do you know how to do it?
-            </div>
-            <div className={style.item}>
-                At 14:00 after lunch we will meet on the spot
-            </div>
-            <div className={style.item}>
-                What time will you be home today?
-            </div>
-            <div className={style.item}>
-                I think we should move on
-            </div>
-            <div className={style.item}>
-                the typescript is not that complicated
-            </div>
-            <div className={style.item}>
-                look at my project on github
-            </div>
-            <div className={style.item}>
-                Yes, I will go there
+                {props.message}
             </div>
         </div>
     );
--- src/components/Messenger/Messenger.jsx
+++ src/components/Messenger/Messenger.jsx
@@ -4,6 +4,18 @@ import UserNameDialog from "./UserNameDialog/UserNameDialog";
 import DialogContent from "./DialogContent/DialogContent";
 
 
+const dialogContentData = [
+    {message: "Hi, aren't you studying react right now?"},
+    {message: "Do you know how to do it?"},
+    {message: "At 14:00 after lunch we will meet on the spot"},
+    {message: "What time will you be home today?"},
+    {message: "I think we should move on"},
+    {message: "the typescript is not that complicated"},
+    {message: "look at my project on github"},
+    {message: "Yes, I will go there"},
+]
+
+
 const Messenger = () => {
     return (
         <div className={style.messenger}>
@@ -11,7 +23,14 @@ const Messenger = () => {
 
             <div className={style.dialog_window}>
                 <UserNameDialog />
-                <DialogContent />
+                <DialogContent message={dialogContentData [0].message} />
+                <DialogContent message={dialogContentData [1].message} />
+                <DialogContent message={dialogContentData [2].message} />
+                <DialogContent message={dialogContentData [3].message} />
+                <DialogContent message={dialogContentData [4].message} />
+                <DialogContent message={dialogContentData [5].message} />
+                <DialogContent message={dialogContentData [6].message} />
+                <DialogContent message={dialogContentData [7].message} />
             </div>
 
         </div>
