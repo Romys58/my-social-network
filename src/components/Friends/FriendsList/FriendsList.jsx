c65b30b60bb967574d6e2b1473a66e710f5066c0
unknown <rom44ain.filippov@yandex.ru>
2022-04-13 22:47:55 +0300

    add data in friendsList

--- src/components/Friends/FriendsList/FriendsList.jsx
+++ src/components/Friends/FriendsList/FriendsList.jsx
@@ -3,17 +3,29 @@ import style from "./FriendsList.module.css";
 import FriendsUserRow from "./FriendsUserRow/FriendsUserRow";
 
 
+const friendsUserRowData = [
+    {name: "Evgenia Saunina", info: "education center, college for makeup artists and dipilation"},
+    {name: "Kirill Derevyashkin", info: "Penza College of Information and Industrial Technologies"},
+    {name: "Alexey Astashkin", info: "Penza College of Food Industry and Commerce (ex. PL 21, PL 42)"},
+    {name: "Maxim Krivulin", info: "PI im. Belinsky PSU (ex. PGPU)"},
+    {name: "Denis Bazyaskin", info: "Penza State Polytechnic College (formerly Instrument-making College"},
+    {name: "Evgeny Krasnobaev", info: "IP Krasnobaev IT. Cabinet furniture from the manufacturer"},
+    {name: "Olga Subbotina", info: "Cutting and sewing"},
+    {name: "Anastasia Kolotkova", info: "Internet entrepreneur"},
+]
+
+
 const FriendsList = () => {
     return(
         <div className={style.friends__list}>
-                <FriendsUserRow name="Evgenia Saunina" info="education center, college for makeup artists and dipilation" />
-                <FriendsUserRow name="Kirill Derevyashkin" info="Penza College of Information and Industrial Technologies" />
-                <FriendsUserRow name="Alexey Astashkin" info="Penza College of Food Industry and Commerce (ex. PL 21, PL 42)" />
-                <FriendsUserRow name="Maxim Krivulin" info="PI im. Belinsky PSU (ex. PGPU)" />
-                <FriendsUserRow name="Denis Bazyaskin" info="Penza State Polytechnic College (formerly Instrument-making College" />
-                <FriendsUserRow name="Evgeny Krasnobaev" info="IP Krasnobaev IT. Cabinet furniture from the manufacturer" />
-                <FriendsUserRow name="Olga Subbotina" info="Cutting and sewing" />
-                <FriendsUserRow name="Anastasia Kolotkova" info="Internet entrepreneur" />
+                <FriendsUserRow name={friendsUserRowData [0].name} info={friendsUserRowData [0].info} />
+                <FriendsUserRow name={friendsUserRowData [1].name} info={friendsUserRowData [1].info} />
+                <FriendsUserRow name={friendsUserRowData [2].name} info={friendsUserRowData [2].info} />
+                <FriendsUserRow name={friendsUserRowData [3].name} info={friendsUserRowData [3].info} />
+                <FriendsUserRow name={friendsUserRowData [4].name} info={friendsUserRowData [4].info} />
+                <FriendsUserRow name={friendsUserRowData [5].name} info={friendsUserRowData [5].info} />
+                <FriendsUserRow name={friendsUserRowData [6].name} info={friendsUserRowData [6].info} />
+                <FriendsUserRow name={friendsUserRowData [7].name} info={friendsUserRowData [7].info} />
             </div>
     );
 }
