2ff85944cd7c776095848fb4a6ff10293132b9ab
Romys58 <ro44ain.filippov@yandex.ru>
2022-05-11 18:33:14 +0300

    new

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,55 +9,55 @@ const Navbar = () => {
               <div className={style.navbar}>
                      <div className={style.item}>
                             <NavLink className={style.link} to="/profile">
-                                   <FontAwesomeIcon className={style.icon} icon="fa-regular fa-circle-user" transform="left-7" size="xl" />
+                                   <FontAwesomeIcon className={style.icon} icon="fa-regular fa-circle-user" transform="left-2" size="xl" />
                                    Profile
                             </NavLink>
                      </div>
                      <div className={style.item}>
                             <NavLink className={style.link} to="/news">
-                                   <FontAwesomeIcon className={style.icon} icon="fa-regular fa-newspaper" transform="left-7" size="xl" />
+                                   <FontAwesomeIcon className={style.icon} icon="fa-regular fa-newspaper" transform="left-2" size="xl" />
                                    News
                             </NavLink>
                      </div>
                      <div className={style.item}>
                             <NavLink className={style.link} to="/messenger">
-                                   <FontAwesomeIcon className={style.icon} icon="fa-regular fa-comment" transform="left-7" size="xl" />
+                                   <FontAwesomeIcon className={style.icon} icon="fa-regular fa-comment" transform="left-2" size="xl" />
                                    Messenger
                             </NavLink>
                      </div>
                      <div className={style.item}>
                             <NavLink className={style.link} to="/friends">
-                                   <FontAwesomeIcon className={style.icon} icon="fa-solid fa-user-group" transform="left-7" size="xl" />
+                                   <FontAwesomeIcon className={style.icon} icon="fa-solid fa-user-group" transform="left-2" size="xl" />
                                    Friends
                             </NavLink>
                      </div>
                      <div className={style.item}>
                             <NavLink className={style.link} to="/communities">
-                                   <FontAwesomeIcon className={style.icon} icon="fa-solid fa-people-group" transform="left-7" size="xl" />
+                                   <FontAwesomeIcon className={style.icon} icon="fa-solid fa-people-group" transform="left-2" size="xl" />
                                    Communities
                             </NavLink>
                      </div>
                      <div className={style.item}>
                             <NavLink className={style.link} to="/photo">
-                                   <FontAwesomeIcon className={style.icon} icon="fa-regular fa-image" transform="left-7" size="xl" />
+                                   <FontAwesomeIcon className={style.icon} icon="fa-regular fa-image" transform="left-2" size="xl" />
                                    Photo
                             </NavLink>
                      </div>
                      <div className={style.item}>
                             <NavLink className={style.link} to="/music">
-                                   <FontAwesomeIcon className={style.icon} icon="fa-solid fa-music" transform="left-7" size="xl" />
+                                   <FontAwesomeIcon className={style.icon} icon="fa-solid fa-music" transform="left-2" size="xl" />
                                    Music
                             </NavLink>
                      </div>
                      <div className={style.item}>
                             <NavLink className={style.link} to="/videos">
-                                   <FontAwesomeIcon className={style.icon} icon="fa-solid fa-video" transform="left-7" size="xl" />
+                                   <FontAwesomeIcon className={style.icon} icon="fa-solid fa-video" transform="left-2" size="xl" />
                                    Videos
                             </NavLink>
                      </div>
                      <div className={style.item}>
                             <NavLink className={style.link} to="/settings">
-                                   <FontAwesomeIcon className={style.icon} icon="fa-solid fa-gears" transform="left-7" size="xl" />
+                                   <FontAwesomeIcon className={style.icon} icon="fa-solid fa-gears" transform="left-2" size="xl" />
                                    Settings
                             </NavLink>
                      </div>
